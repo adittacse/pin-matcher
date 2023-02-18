5e2a1d0669c0673768e7f030cbf1c29e4bd5ee87
Aditta Chakraborty <adittacse@gmail.com>
2023-02-18 08:29:09 +0600

    generate pin matching function added

--- js/app.js
+++ js/app.js
@@ -18,3 +18,41 @@ document.getElementById("generate-pin").addEventListener("click", function() {
     const displayPinField = document.getElementById("display-pin");
     displayPinField.value = pin;
 });
+
+document.getElementById("calculator").addEventListener("click", function(event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById("typed-numbers");
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === "C") {
+            typedNumberField.value = "";
+        } else if (number === "<") {
+            const digits = previousTypedNumber.split("");
+            digits.pop();
+            const remainingDigits = digits.join("");
+            typedNumberField.value = remainingDigits;
+        }
+    } else {
+        const newTypedNumber = previousTypedNumber + number
+        typedNumberField.value = newTypedNumber;
+    }
+});
+
+document.getElementById("btn-verify-pin").addEventListener("click", function() {
+    const displayPinField = document.getElementById("display-pin");
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById("typed-numbers");
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccessMessage = document.getElementById("pin-success");
+    const pinFailureMessage = document.getElementById("pin-failure");
+
+    if(typedNumber === currentPin) {
+        pinFailureMessage.style.display = "none";
+        pinSuccessMessage.style.display = "block";
+    } else {
+        pinSuccessMessage.style.display = "none";
+        pinFailureMessage.style.display = "block";
+    }
+});
